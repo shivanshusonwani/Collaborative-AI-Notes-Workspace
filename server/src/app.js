11a4609da387eb2832b506bb12c4113c85498208
ShivanshuSonwani <shivanshu.ed@gmail.com>
2026-05-15 02:15:55 +0530

    add express-session with MongoDB session store

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,8 @@
 import express from "express";
+import sessionConfig from "./config/session.js";
 
 const app = express();
+
+app.use(sessionConfig);
 
 export default app;
---
+++ server/src/config/session.js
@@ -0,0 +1,21 @@
+import session from "express-session";
+import MongoStore from "connect-mongo";
+
+const sessionConfig = session({
+	name: "sid",
+	secret: process.env.SESSION_SECRET,
+	resave: false,
+	saveUninitialized: false,
+	cookie: {
+		maxAge: 1000 * 60 * 60 * 24, // 1 day
+		httpOnly: true,
+		secure: process.env.NODE_ENV === "production",
+		sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+	},
+	store: MongoStore.create({
+		mongoUrl: process.env.MONGODB_URI,
+		collectionName: "sessions",
+	}),
+});
+
+export default sessionConfig;
