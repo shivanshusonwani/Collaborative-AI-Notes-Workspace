b34057406bb48b3dde8728ca026d3d20f666675e
ShivanshuSonwani <shivanshu.ed@gmail.com>
2026-05-15 02:16:46 +0530

    add health check endpoint

--- server/src/app.js
+++ server/src/app.js
@@ -5,4 +5,11 @@ const app = express();
 
 app.use(sessionConfig);
 
+app.get("/health", (req, res) => {
+	res.status(200).json({
+		status: "OK",
+		message: "server is up and running.",
+	});
+});
+
 export default app;
