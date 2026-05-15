312aa8a5cf3c96b38aa82a9696b29e256bd06753
ShivanshuSonwani <shivanshu.ed@gmail.com>
2026-05-15 22:25:07 +0530

    initialize react project with vite, and add tailwindcss

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+	return (
+		<>
+			<div className='h-screen flex justify-center items-center'>
+				<h1 className='max-w-2xl text-6xl font-bold text-violet-600'>
+					Collaborative AI Notes Workspace
+				</h1>
+			</div>
+		</>
+	);
+}
+
+export default App;
